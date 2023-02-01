5b68abc7d428eca7472d65658f3b81a316dabb3e
student <student@ioe-aids-183>
2023-02-01 13:58:05 +0530

    first commit msg

---
+++ Dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
